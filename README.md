0aea553dcb63db2e870e5741fb1ac6448f570733
pushebp <pushebp@163.com>
2017-05-22 10:57:19 +0800

    upgrade gradle to 2.3.2  + add readme.md + reformat code

--- README.md
+++ README.md
@@ -1,8 +1,190 @@
-# captcha-android-demo
-易盾验证码android应用嵌入演示
+﻿# 易盾验证码Android SDK接入指南
 
-# 演示截图
+## 一、SDK集成
+### 1、自动导入（推荐）
+在Module的build.gradle文件中添加依赖和属性配置：
+```
+dependencies {
+    compile xxxxxxx //如果指定版本号升级版本后请clean下工程
+}
+```
 
-<img src="https://github.com/yidun/captcha-android-demo/raw/master/screenshots/Screenshot_1482991322.png" width="30%" height="30%">
-<img src="https://github.com/yidun/captcha-android-demo/raw/master/screenshots/Screenshot_1482991359.png" width="30%" height="30%">
-<img src="https://github.com/yidun/captcha-android-demo/raw/master/screenshots/Screenshot_1482991371.png" width="30%" height="30%">
+### 2、手动导入
+直接将jar包导入到工程目录下
+
+## 二、SDK接口
+```
+//可以自定义deviceid，若不填写则默认获取手机的imei值
+public void setDeviceId();  
+
+//可以自定义sdk框架超时时间，单位毫秒，默认是10000即10秒
+public void setTimeout();
+
+//是否开启调试，设置为true后可以看到部分调试信息
+public void setDebug();
+
+//简单测试是否填写captcha 是否初始化，包括captchaId与设置监听对象
+public boolean checkParams();
+```
+
+## 三、集成说明
+### 1、初始化
+```
+Captcha mCaptcha = new Captcha(context);
+
+//设置CaptchaId：这里填入从易盾官网申请到的验证码id
+mCaptcha.setCaptchaId(captchaid);
+
+//设置监听对象captchaListener
+mCaptcha.setCaListener(captchalistener);
+
+
+//其中captchalistener接口形式：
+public interface CaptchaListener {
+    //通知验证已准备完毕,true准备完成/false未准备完成
+    void onReady(Boolean status); 
+
+    //通知native关闭验证
+    void closeWindow();
+
+    //通知javascript发生严重错误，msg为错误信息
+    void onError(String msg);
+
+    //通知验证结果，其中validate值为返回值。可以在该函数中进行用户自定义二次校验
+    void onValidate(String result, String validate, String message);
+
+    //用户取消验证
+    void onCancel();
+}
+```
+
+示例：
+```
+/*验证码SDK,该Demo采用异步获取方式*/
+private UserLoginTask mLoginTask = null;
+//自定义Listener格式如下
+CaptchaListener myCaptchaListener = new CaptchaListener() {
+
+    @Override
+    public void onValidate(String result, String validate, String message) {
+        //验证结果，valiadte，可以根据返回的三个值进行用户自定义二次验证
+        if (validate.length() > 0) {
+            toastMsg("验证成功，validate = " + validate);
+        } else {
+            toastMsg("验证失败：result = " + result + ", validate = " + validate + ", message = " + message);
+
+        }
+    }
+
+    @Override
+    public void closeWindow() {
+        //请求关闭页面
+        toastMsg("关闭页面");
+    }
+
+    @Override
+    public void onError(String errormsg) {
+        //出错
+        toastMsg("错误信息：" + errormsg);
+    }
+
+    @Override
+    public void onCancel() {
+        toastMsg("取消线程");
+        //用户取消加载或者用户取消验证，关闭异步任务，也可根据情况在其他地方添加关闭异步任务接口
+        if (mLoginTask != null) {
+            if (mLoginTask.getStatus() == AsyncTask.Status.RUNNING) {
+                Log.i(TAG, "stop mLoginTask");
+                mLoginTask.cancel(true);
+            }
+        }
+    }
+
+    @Override
+    public void onReady(boolean ret) {
+        //该为调试接口，ret为true表示加载Sdk完成
+        if (ret) {
+            toastMsg("验证码sdk加载成功");
+        }
+    }
+
+};
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	....
+	....
+
+    //初始化验证码SDK相关参数，设置CaptchaId、Listener最后调用start初始化。
+    if (mCaptcha == null) {
+        mCaptcha = new Captcha(mContext);
+    }
+    mCaptcha.setCaptchaId(testCaptchaId);
+    mCaptcha.setCaListener(myCaptchaListener);
+    //可选：开启debug
+    mCaptcha.setDebug(false);
+    //可选：设置超时时间
+    mCaptcha.setTimeout(10000);
+    //登陆操作
+    button_login.setOnClickListener(new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mLoginTask = new UserLoginTask();
+            //关闭mLoginTask任务可以放在myCaptchaListener的onCancel接口中处理
+            mLoginTask.execute();
+            //必填：初始化 captcha框架
+            mCaptcha.start();
+            //可直接调用验证函数Validate()，本demo采取在异步任务中调用（见UserLoginTask类中）
+            //mCaptcha.Validate();
+        }
+    });
+
+}
+
+public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+
+    UserLoginTask() {
+    }
+
+    @Override
+    protected Boolean doInBackground(Void... params) {
+        //可选：简单验证DeviceId、CaptchaId、Listener值
+        return mCaptcha.checkParams();
+    }
+
+    @Override
+    protected void onPostExecute(final Boolean success) {
+        if (success) {
+            //必填：开始验证
+            mCaptcha.Validate();
+        } else {
+            toastMsg("验证码SDK参数设置错误,请检查配置");
+        }
+    }
+
+    @Override
+    protected void onCancelled() {
+        mLoginTask = null;
+    }
+}
+```
+### 2、弹出验证码
+```
+//启动验证码sdk框架，主要为loading界面
+public void start();
+
+//开始验证，主要为验证码界面
+public void Validate();
+```
+
+示例
+```
+mCaptcha.start();
+//可直接调用验证函数Validate()，本demo采取在异步任务中调用（见UserLoginTask类中）
+//mCaptcha.Validate();
+```
+
+## 效果演示
+![](https://github.com/yidun/captcha-android-demo/raw/master/screenshots/Screenshot_1482991322.png)
+![](https://github.com/yidun/captcha-android-demo/raw/master/screenshots/Screenshot_1482991359.png)
+![](https://github.com/yidun/captcha-android-demo/raw/master/screenshots/Screenshot_1482991371.png)
--- app/src/main/java/nis/netease/com/captcha/LoginActivity.java
+++ app/src/main/java/nis/netease/com/captcha/LoginActivity.java
@@ -1,6 +1,5 @@
 package nis.netease.com.captcha;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -20,63 +19,25 @@ import com.netease.nis.captcha.CaptchaListener;
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity{
-
-    private final  static String TAG = "Captcha";
+public class LoginActivity extends AppCompatActivity {
+    private final static String TAG = "Captcha";
+    Captcha mCaptcha = null;
     // UI references.
-    private AutoCompleteTextView mEmailView;
-    private EditText mPasswordView;
-    private Button signInButton;
-    private ProgressDialog progressDialog;
-    private Context mcontext= LoginActivity.this;
+    private AutoCompleteTextView text_email;
+    private EditText edit_password;
+    private Button button_login;
+    private Context mContext = null;
     /*验证码SDK,该Demo采用异步获取方式*/
     private UserLoginTask mLoginTask = null;
-    Captcha captcha = null;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        setLoginView();
-
-        String testCaptchaId = "0489d7c00eff49089c56dfcd4b67f250"; // TODO: 这里填入从易盾官网申请到的验证码id
-
-        //初始化验证码SDK相关参数，设置CaptchaId、Listener最后调用start初始化。
-        if (captcha==null){
-            captcha = new Captcha(mcontext);
-        }
-        captcha.setCaptchaId(testCaptchaId);
-        captcha.setCaListener(mytestCaListener);
-        //可选：开启debug
-        captcha.setDebug(false);
-        //可选：设置超时时间
-        captcha.setTimeout(10000);
-        //登陆操作
-        signInButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mLoginTask = new UserLoginTask();
-                //关闭mLoginTask任务可以放在mytestCaListener的onCancel接口中处理
-                mLoginTask.execute();
-                //必填：初始化 captcha框架
-                captcha.start();
-                //可直接调用验证函数Validate()，本demo采取在异步任务中调用（见UserLoginTask类中）
-               //captcha.Validate();
-            }
-        });
-
-    }
-
     //自定义Listener格式如下
-    CaptchaListener mytestCaListener  = new CaptchaListener() {
+    CaptchaListener myCaptchaListener = new CaptchaListener() {
 
         @Override
         public void onValidate(String result, String validate, String message) {
             //验证结果，valiadte，可以根据返回的三个值进行用户自定义二次验证
-            if(validate.length()>0){
-                toastMsg("验证成功，validate = "+validate);
-            }else{
+            if (validate.length() > 0) {
+                toastMsg("验证成功，validate = " + validate);
+            } else {
                 toastMsg("验证失败：result = " + result + ", validate = " + validate + ", message = " + message);
 
             }
@@ -91,14 +52,14 @@ public class LoginActivity extends AppCompatActivity{
         @Override
         public void onError(String errormsg) {
             //出错
-            toastMsg("错误信息："+errormsg);
+            toastMsg("错误信息：" + errormsg);
         }
 
         @Override
         public void onCancel() {
             toastMsg("取消线程");
             //用户取消加载或者用户取消验证，关闭异步任务，也可根据情况在其他地方添加关闭异步任务接口
-            if(mLoginTask!=null){
+            if (mLoginTask != null) {
                 if (mLoginTask.getStatus() == AsyncTask.Status.RUNNING) {
                     Log.i(TAG, "stop mLoginTask");
                     mLoginTask.cancel(true);
@@ -109,13 +70,63 @@ public class LoginActivity extends AppCompatActivity{
         @Override
         public void onReady(boolean ret) {
             //该为调试接口，ret为true表示加载Sdk完成
-            if(ret){
+            if (ret) {
                 toastMsg("验证码sdk加载成功");
             }
         }
 
     };
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        String testCaptchaId = "0489d7c00eff49089c56dfcd4b67f250"; // TODO: 这里填入从易盾官网申请到的验证码id
+
+        mContext = this;
+        initLoginView();
+
+        //初始化验证码SDK相关参数，设置CaptchaId、Listener最后调用start初始化。
+        if (mCaptcha == null) {
+            mCaptcha = new Captcha(mContext);
+        }
+        mCaptcha.setCaptchaId(testCaptchaId);
+        mCaptcha.setCaListener(myCaptchaListener);
+        //可选：开启debug
+        mCaptcha.setDebug(false);
+        //可选：设置超时时间
+        mCaptcha.setTimeout(10000);
+        //登陆操作
+        button_login.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLoginTask = new UserLoginTask();
+                //关闭mLoginTask任务可以放在myCaptchaListener的onCancel接口中处理
+                mLoginTask.execute();
+                //必填：初始化 captcha框架
+                mCaptcha.start();
+                //可直接调用验证函数Validate()，本demo采取在异步任务中调用（见UserLoginTask类中）
+                //mCaptcha.Validate();
+            }
+        });
+
+    }
+
+    //初始化登陆页面
+    private void initLoginView() {
+        text_email = (AutoCompleteTextView) findViewById(R.id.text_email);
+        edit_password = (EditText) findViewById(R.id.edit_password);
+        button_login = (Button) findViewById(R.id.button_login);
+        text_email.setText("dev@dun.163.com");
+        text_email.setInputType(InputType.TYPE_NULL);
+        edit_password.setText("******");
+        edit_password.setInputType(InputType.TYPE_NULL);
+    }
+
+    private void toastMsg(String msg) {
+        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+    }
 
     /**
      * Represents an asynchronous login/registration task used to authenticate
@@ -129,14 +140,14 @@ public class LoginActivity extends AppCompatActivity{
         @Override
         protected Boolean doInBackground(Void... params) {
             //可选：简单验证DeviceId、CaptchaId、Listener值
-            return captcha.checkParams();
+            return mCaptcha.checkParams();
         }
 
         @Override
         protected void onPostExecute(final Boolean success) {
             if (success) {
                 //必填：开始验证
-                captcha.Validate();
+                mCaptcha.Validate();
             } else {
                 toastMsg("验证码SDK参数设置错误,请检查配置");
             }
@@ -147,20 +158,4 @@ public class LoginActivity extends AppCompatActivity{
             mLoginTask = null;
         }
     }
-
-    //初始化登陆页面
-    private void setLoginView(){
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        mPasswordView = (EditText) findViewById(R.id.password);
-        signInButton = (Button) findViewById(R.id.email_sign_in_button);
-        mEmailView.setText("dev@dun.163.com");
-        mEmailView.setInputType(InputType.TYPE_NULL);
-        mPasswordView.setText("******");
-        mPasswordView.setInputType(InputType.TYPE_NULL);
-    }
-
-    private void toastMsg(String msg) {
-        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
-    }
 }
-
