7ef1ce3cf22d17b8d8d569fe68663324f04a164a
hq_sunshine <1294600609@qq.com>
2017-11-02 10:50:54 +0800

    修改和网络相关的某些提示语句，使其更容易区分是哪种网络状况

--- captcha/src/main/java/com/netease/nis/captcha/Captcha.java
+++ captcha/src/main/java/com/netease/nis/captcha/Captcha.java
@@ -281,11 +281,11 @@ public class Captcha {
                         break;
                     case VALIDATETIMEOUT:
                         mp.setCanceledOnTouchOutside(true);
-                        mp.setMessage("网络异常，请检查网络后重试");
+                        mp.setMessage("验证超时，请关闭并检查网络");
                         break;
                     case INITTIMEOUT:
                         mp.setCanceledOnTouchOutside(true);
-                        mp.setMessage("网络异常，请检查网络后重试");
+                        mp.setMessage("初始化超时，请关闭并检查网络");
                         break;
                     default:
                         break;
